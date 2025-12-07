41f06a2303bfebe5812069b984816c119e27749a
MaheshAvhad18 <mahimavhad2512@gmail.com>
2025-12-07 19:36:52 +0530

    updated settings.py for deployent

--- core/settings.py
+++ core/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATICFILES_DIRS = []
 # Whitenoise static optimization
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
